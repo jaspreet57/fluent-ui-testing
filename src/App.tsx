7f3e7fb230267dedda1e3e249f9f2cef30ec078b
Jaspreet Singh <erjaspreet57@gmail.com>
2020-04-16 15:06:45 +0530

    Feat: Add two pivot menus and render another div using their keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,101 @@
-import React, { FormEvent } from "react";
-import {
-  Stack,
-  Text,
-  Link,
-  FontWeights,
-  IDropdownOption,
-} from "office-ui-fabric-react";
-
+import React, { FormEvent, useState } from "react";
+import { Stack, FontWeights, Pivot, PivotItem } from "office-ui-fabric-react";
 import { DropDownWrapper } from "./components/DropDown/DropDownWrapper";
 
-const boldStyle = { root: { fontWeight: FontWeights.semibold } };
+const defaultOptions = [
+  { key: "A", text: "Option a", title: "I am option a." },
+  { key: "B", text: "Option b" },
+  { key: "C", text: "Option c", disabled: true },
+  { key: "D", text: "Option d" },
+  { key: "E", text: "Option e" },
+];
 
 export const App: React.FunctionComponent = () => {
-  const handleChange = (
-    event: FormEvent<HTMLDivElement>,
-    item?: IDropdownOption | undefined,
-  ): void => {
-    console.log("current target", item);
+  const [topMenuKey, setTopMenuKey] = useState<string>("custom");
+  const [innerMenuKey, setInnerMenuKey] = useState<string>("one");
+
+  const onTopMenuClick = (item: PivotItem | undefined): void => {
+    if (!item || !item.props.itemKey) {
+      return;
+    }
+    setTopMenuKey(item.props.itemKey);
   };
 
-  const options = [
-    { key: "A", text: "Option a", title: "I am option a." },
-    { key: "B", text: "Option b" },
-    { key: "C", text: "Option c", disabled: true },
-    { key: "D", text: "Option d" },
-    { key: "E", text: "Option e" },
-  ];
+  const onInnerMenuClick = (item: PivotItem | undefined): void => {
+    if (!item || !item.props.itemKey) {
+      return;
+    }
+    setInnerMenuKey(item.props.itemKey);
+  };
 
   return (
     <Stack
-      horizontalAlign="center"
-      verticalAlign="center"
       verticalFill
       styles={{
         root: {
           width: "960px",
           margin: "0 auto",
-          textAlign: "center",
           color: "#605e5c",
         },
       }}
       gap={15}
     >
-      <img
-        src="https://raw.githubusercontent.com/Microsoft/just/master/packages/just-stack-uifabric/template/src/components/fabric.png"
-        alt="logo"
-      />
-      <Text variant="xxLarge" styles={boldStyle}>
-        Welcome to Your UI Fabric App
-      </Text>
-      <DropDownWrapper handleChange={handleChange} options={options}/>
-      <Text variant="large">
-        For a guide on how to customize this project, check out the UI Fabric
-        documentation.
-      </Text>
-      <Text variant="large" styles={boldStyle}>
-        Essential Links
-      </Text>
-      <Stack horizontal gap={15} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">
-          Stack Overflow
-        </Link>
-        <Link href="https://github.com/officeDev/office-ui-fabric-react/">
-          Github
-        </Link>
-        <Link href="https://twitter.com/officeuifabric">Twitter</Link>
+      <div style={{ borderBottom: "1px solid #c8c8c8" }}>
+        <Pivot
+          aria-label="Top Menu"
+          selectedKey={topMenuKey}
+          onLinkClick={onTopMenuClick}
+          headersOnly={true}
+        >
+          <PivotItem headerText="Custom" itemKey="custom" />
+          <PivotItem headerText="Support" itemKey="support" />
+        </Pivot>
+      </div>
+      <Stack horizontal verticalAlign="center" gap={10}>
+        <div>Label here :</div>
+        <DropDownWrapper handleChange={() => {}} options={defaultOptions} />
       </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design System
-      </Text>
-      <Stack horizontal gap={15} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">
-          Icons
-        </Link>
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">
-          Typography
-        </Link>
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">
-          Theme
-        </Link>
+
+      <div style={{ borderBottom: "1px solid #c8c8c8" }}>
+        <Pivot
+          aria-label="Inner Menu"
+          selectedKey={innerMenuKey}
+          onLinkClick={onInnerMenuClick}
+          headersOnly={true}
+        >
+          <PivotItem headerText="One" itemKey="one" />
+          <PivotItem headerText="Two" itemKey="two" />
+          <PivotItem headerText="Three" itemKey="three" />
+          <PivotItem headerText="Four" itemKey="four" />
+          <PivotItem headerText="Five" itemKey="five" />
+          <PivotItem headerText="Six" itemKey="six" />
+        </Pivot>
+      </div>
+      <Stack horizontal gap={5}>
+        <Stack.Item
+          grow
+          styles={{
+            root: {
+              borderRight: "1px solid #c8c8c8",
+              width: 100,
+              height: 100,
+            },
+          }}
+        >
+          <div> Table here</div>
+        </Stack.Item>
+        <Stack.Item
+          styles={{
+            root: {
+              padding: 0,
+              paddingTop: 40,
+
+              width: 200,
+            },
+          }}
+        >
+          View Area
+        </Stack.Item>
       </Stack>
     </Stack>
   );
